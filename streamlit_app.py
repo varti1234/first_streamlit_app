878e526612e281662e706b0e6dc6fab67a141ee6
Vartika <122364288+varti1234@users.noreply.github.com>
2023-01-11 12:18:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title('My parents New Healthly Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and Blueberry')
+streamlit.text('Kale,Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
