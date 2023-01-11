84af68a658707413f8e16e7010888cfdfbe60e1e
Vartika <122364288+varti1234@users.noreply.github.com>
2023-01-11 12:25:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 
 import streamlit
 streamlit.title('My parents New Healthly Diner')
-streamlit.header('🍌🥭 Build your own Fruit Smoothie 🥝🍇')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Blueberry')
 streamlit.text('Kale,Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build your own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
