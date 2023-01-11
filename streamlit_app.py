2588a4db5bd2865b5904283baeb3fb4be4470ce0
Vartika <122364288+varti1234@users.noreply.github.com>
2023-01-11 12:30:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ streamlit.text('Omega 3 and Blueberry')
 streamlit.text('Kale,Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build your own Fruit Smoothie 🥝🍇')
+
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=streamlit.set_index('Fruit')
@@ -14,4 +15,4 @@ my_fruit_list=streamlit.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-Streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
