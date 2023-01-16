3020e277b68e913995cf9761acc16c14038c2a54
Vartika <122364288+varti1234@users.noreply.github.com>
2023-01-16 14:34:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My parents New Healthly Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry')
-streamlit.text('Kale,Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build your own Fruit Smoothie 🥝🍇')
-
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
-
 streamlit.header('Fruityvice Fruit Advice!')
 try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -36,18 +18,3 @@ if not fruit_choice:
       except URLError as e:
         streamlit.error()
         
-
-
-streamlit.stop()
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("Fruit Load list contains:")
-streamlit.dataframe(my_data_rows)
-add_my_fruit=streamlit.text_input('What fruit would you like to add?','Jackfruit') 
-streamlit.write('The user entered ', add_my_fruit)
-
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
