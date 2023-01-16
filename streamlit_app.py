257cda1bca590c1cc0b00aa5bcbc15c7e542c3ec
Vartika <122364288+varti1234@users.noreply.github.com>
2023-01-16 14:46:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from urllib.error import URLError
 
 #create the repeatable code block(called a function)
 def get_fruityvice_data(this_fruit_choice):
-   fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
   
